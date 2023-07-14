a9b971e754c01e446f53c511a7489db1eb633bdb
Samuel Hale <samuelhalebusiness@gmail.com>
2023-07-14 13:25:17 -0400

    Implemented and tested A star algorithm

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,11 +14,11 @@ using namespace std;
 // 3 South
 vector<int> cost = {2, 1, 2, 3};
 
+string curS = "00";
+
 int gi = 0, gj = 0;
 
-vector<vector<string>> maze;
-
-void searchMaze(int &i, int&j, string sN) {
+void searchMaze(int &i, int&j, string sN, vector<vector<string>> maze) {
 	for (i = 0; i < maze.size(); i++) {
 		for (j = 0; j < maze[i].size(); j++) {
 			if (maze[i][j] == sN) {
@@ -29,15 +30,32 @@ void searchMaze(int &i, int&j, string sN) {
 }
 
 // Function to print the maze
-void printMaze() {
+void printMaze(vector<vector<string>> maze) {
 	for (int i = 0; i < maze.size(); i++) {
 		for (int j = 0; j < maze[i].size(); j++) {
 			cout << maze[i][j] << " ";
 		}
 		cout << endl;
 	}
+	cout << endl;
 }
 
+// Function that takes in a string that represents a double digit that can lead with 0 and increments it 
+// by 1 for example "00" would become "01"
+string increment(string s) {
+	stringstream ss;
+	int i;
+	ss << s;
+	ss >> i;
+	i++;
+	ss.clear();
+	ss << i;
+	ss >> s;
+	if (s.length() == 1) {
+		s = "0" + s;
+	}
+	return s;
+}
 
 /* Got the below tree code from this source
 https://kalkicode.com/n-ary-tree-node-insertion-in-cpp
@@ -70,15 +88,14 @@ class Step
         this->visited = visited;
     }
 
-	void addChild(string sN, int f = 0, int g = 0, int h = 0, bool visited = false)
+	void addChild(string sN, int f = 0, int g = 0, int h = 0, bool visited = false, vector<vector<string>> maze = {})
 	{
 		Step *t = new Step(sN, f, g, h, visited, this);
-		t->heuristic();
-		t->test_heuristic();
+		t->heuristic(maze);
 		this->child.push_back(t);
 	}
 
-	void heuristic() {
+	void heuristic(vector<vector<string>> maze) {
 		int pi = 0;
 		int pj = 0;
 		int ci = 0;
@@ -92,14 +109,13 @@ class Step
 		/* CALCULATE G */
 		// Find parent and childs index
 		if (this->parent != NULL) {
-			searchMaze(pi, pj, this->parent->sN);
+			searchMaze(pi, pj, this->parent->sN, maze);
 		}
-		searchMaze(ci, cj, this->sN);
+		searchMaze(ci, cj, this->sN, maze);
 		if (this->parent != NULL) {
 			// Calculate step cost and set g
 			if (pi != ci) {
-				ci -= pi;
-				if (ci == 1) {
+				if (ci - pi == 1) {
 					if (this->parent != NULL) {
 						// Child is south
 						this->g = this->parent->g + cost[3];
@@ -115,8 +131,7 @@ class Step
 				}
 			}
 			if (pj != cj) {
-				cj -= pj;
-				if (cj == 1) {
+				if (cj - pj == 1) {
 					if (this->parent != NULL) {
 						// Child is east
 						this->g = this->parent->g + cost[2];
@@ -168,35 +183,28 @@ class Step
 	}
 };
 
-
-
-class NAryTree
-{
-	public: 
-    Step *root;
-	NAryTree()
-	{
-		this->root = nullptr;
-	}
-	void printPreorder(Step *node)
-	{
-		if (node == nullptr)
-		{
-			return;
-		}
-		int i = 0;
-		Step *temp = nullptr;
-		cout << "  " << node->sN;
-		// iterating the child of given node
-		while (i < node->child.size())
-		{
-			temp = node->child.at(i);
-			this->printPreorder(temp);
-			i++;
+void sortSteps(vector<Step*> &steps) {
+	// sort steps by f value and if a tie occurs sort by sN value
+	Step *temp = NULL;
+	for (int i = 0; i < steps.size(); i++) {
+		for (int j = i + 1; j < steps.size(); j++) {
+			if (steps[i]->f > steps[j]->f) {
+				temp = steps[i];
+				steps[i] = steps[j];
+				steps[j] = temp;
+			}
+			else if (steps[i]->f == steps[j]->f) {
+				if (steps[i]->sN > steps[j]->sN) {
+					temp = steps[i];
+					steps[i] = steps[j];
+					steps[j] = temp;
+				}
+			}
 		}
 	}
-};
 
+
+}
 
 
 vector<vector<string>> initMaze(vector<vector<string>> m) {
@@ -223,8 +231,56 @@ vector<vector<string>> initMaze(vector<vector<string>> m) {
 /*
 param {*} m: A vector of type strign that represents our maze
 param {*} s: A vector of ADT Step that represent*/
-vector<vector<string>> aStar(vector<vector<string>>& m, NAryTree *tree) {
-
+vector<vector<string>> aStar(vector<vector<string>> m, Step *s, vector<Step *> fVals) {
+	// Step 1 search around current node in the order of west, north, east, south
+	int curI = 0, curJ = 0;
+	searchMaze(curI, curJ, s->sN, m);
+	// pop the front of fVals
+	s->visited = true;
+	fVals.erase(fVals.begin());
+	// Step 1.1 check if the current node is the goal node
+	if (curI == gi && curJ == gj) {
+		// Step 1.1.1 if it is the goal node then return the maze
+		printMaze(m);
+		return m;
+	}
+	// Step 1.2 if it is not the goal node then add every "[]" value as a child to the current node
+	if (curJ > 0 && m[curI][curJ - 1] == "[]"
+	|| curJ > 0 && m[curI][curJ - 1] == "G") {
+		// Add west child
+		m[curI][curJ - 1] = curS;
+		s->addChild(curS, 0, 0, 0, false, m);
+		curS = increment(curS);
+		// push this new child onto fVals
+		fVals.push_back(s->child[s->child.size() - 1]);
+	}
+	if (curI > 0 && m[curI - 1][curJ] == "[]"
+	|| curI > 0 && m[curI - 1][curJ] == "G") {
+		// Add north child
+		m[curI - 1][curJ] = curS;
+		s->addChild(curS, 0, 0, 0, false, m);
+		curS = increment(curS);
+		fVals.push_back(s->child[s->child.size() - 1]);
+	}
+	if (curJ < m[0].size() - 1 && m[curI][curJ + 1] == "[]"
+	|| curJ < m[0].size() - 1 && m[curI][curJ + 1] == "G") {
+		// Add east child
+		m[curI][curJ + 1] = curS;
+		s->addChild(curS, 0, 0, 0, false, m);
+		curS = increment(curS);
+		fVals.push_back(s->child[s->child.size() - 1]);
+	}
+	if (curI < m.size() - 1 && m[curI + 1][curJ] == "[]"
+	|| curI < m.size() - 1 && m[curI + 1][curJ] == "G") {
+		// Add south child
+		m[curI + 1][curJ] = curS;
+		s->addChild(curS, 0, 0, 0, false, m);
+		curS = increment(curS);
+		fVals.push_back(s->child[s->child.size() - 1]);
+	}
+	sortSteps(fVals); // sort the fVals vector
+	// Step 2 find the unvisited node with the lowest f value
+	aStar(m, fVals[0], fVals);
     return m;
 }
 
@@ -235,14 +291,18 @@ TO DO:
 2) Test
 */
 int main() {
+	vector<vector<string>> maze;
 	int si = 0, sj = 0;
-    NAryTree *tree = new NAryTree();
+	vector<Step *> fVals;
     maze = initMaze(maze);
-	searchMaze(gi, gj, "G"); // initialize global gi and gj
-	searchMaze(si, sj, "S"); // initialize start point
-	maze[si][sj] = "00";
-	Step *root = new Step("00");
-	root->heuristic();
-	delete tree;
+	searchMaze(gi, gj, "G", maze); // initialize global gi and gj
+	searchMaze(si, sj, "S", maze); // initialize start point
+	maze[si][sj] = curS;
+	Step *root = new Step(curS);
+	curS = increment(curS);
+	root->heuristic(maze);
+	fVals.push_back(root);
+	sortSteps(fVals);
+	aStar(maze, root, fVals);
     return 0;
 }
